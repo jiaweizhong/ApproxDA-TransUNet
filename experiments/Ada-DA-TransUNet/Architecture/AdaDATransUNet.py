baf29e518bbfb421d061d8ba7680a2c0c7aed441
jiaweizhong <oracleinscience@gmail.com>
2026-06-13 19:23:18 -0700

    Fix GPU runtime error with .contiguous()

--- experiments/Ada-DA-TransUNet/Architecture/AdaDATransUNet.py
+++ experiments/Ada-DA-TransUNet/Architecture/AdaDATransUNet.py
@@ -73,7 +73,7 @@ class Attention(nn.Module):
     def transpose_for_scores(self, x):
         new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
         x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
+        return x.permute(0, 2, 1, 3).contiguous()
 
     def forward(self, hidden_states):
         mixed_query_layer = self.query(hidden_states)
